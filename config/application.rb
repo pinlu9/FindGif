6341a9f517bfa6277e01ad501e4d053c92a845d8
Pin <pinlu9@gmail.com>
2015-11-30 01:00:24 -0500

    background

--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,5 @@ module Giphyapi
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-  config.assets.paths << "#{Rails.root}/app/assets"
-
-  config.assets.paths << "#{Rails.root}/app"
-  
   end
 end
